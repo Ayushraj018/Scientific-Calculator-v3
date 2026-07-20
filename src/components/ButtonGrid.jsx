5ca1b2e33db6b5b9a6a7fc6f69b30e4632819d54
Ayushraj018 <149755909+Ayushraj018@users.noreply.github.com>
2026-07-20 21:10:36 +0530

    Update ButtonGrid.jsx

--- src/components/ButtonGrid.jsx
+++ src/components/ButtonGrid.jsx
@@ -1,1 +1,54 @@
+import Button from "./Button";
 
+const buttons = [
+  "(",
+  ")",
+  "C",
+  "⌫",
+
+  "sin",
+  "cos",
+  "tan",
+  "/",
+
+  "log",
+  "ln",
+  "√",
+  "^",
+
+  "7",
+  "8",
+  "9",
+  "×",
+
+  "4",
+  "5",
+  "6",
+  "-",
+
+  "1",
+  "2",
+  "3",
+  "+",
+
+  "π",
+  "0",
+  ".",
+  "=",
+];
+
+const ButtonGrid = ({ onButtonClick }) => {
+  return (
+    <div className="grid grid-cols-4 gap-3">
+      {buttons.map((btn) => (
+        <Button
+          key={btn}
+          value={btn}
+          onClick={onButtonClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ButtonGrid;
