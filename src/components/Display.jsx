5c8bdeb1262466563051a5e7e51c203e0e1e9683
Ayushraj018 <149755909+Ayushraj018@users.noreply.github.com>
2026-07-20 21:11:12 +0530

    Update Display.jsx

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,1 +1,25 @@
+const Display = ({ display, angleMode }) => {
+  return (
+    <div className="bg-slate-900 rounded-2xl p-5 shadow-inner">
 
+      <div className="flex justify-between items-center">
+
+        <span className="text-blue-400 font-bold">
+          {angleMode}
+        </span>
+
+        <span className="text-gray-400 text-sm">
+          Scientific Calculator
+        </span>
+
+      </div>
+
+      <div className="text-right text-white text-5xl mt-6 break-all font-light">
+        {display}
+      </div>
+
+    </div>
+  );
+};
+
+export default Display;
